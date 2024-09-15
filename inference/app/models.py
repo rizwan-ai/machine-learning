775a1ff002cc382302c0a3151515087c021c5aca
Muhammad Rizwan <rizwanse66@gmail.com>
2024-09-15 19:29:51 +0500

    🔧 Add default values to FlightInput model fields ✈️

--- inference/app/models.py
+++ inference/app/models.py
@@ -4,13 +4,13 @@ from typing import List, Dict, Any
 
 
 class FlightInput(BaseModel):
-    carrier: str
-    dest: str
-    dep_delay: float
-    arr_delay: float
-    arr_time: float
-    month: float
-    plane_age: float
+    carrier: str = "AS"
+    dest: str = "HNL"
+    dep_delay: float = 5
+    arr_delay: float = 5
+    arr_time: float = 1505
+    month: float = 1
+    plane_age: float = 17
 
 class PredictionResponse(BaseModel):
     prediction: float
